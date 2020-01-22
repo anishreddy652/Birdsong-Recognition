66234bdf81422a3f9888102ec4daff3257e616a5
Anish Reddy <48986416+anishreddy652@users.noreply.github.com>
2020-01-22 06:46:11 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,36 @@
+# coding= UTF-8
+#
+# Author: Fing
+# Date  : 2017-12-03
+#
+import xlsxwriter
+import numpy as np
+import sklearn
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
+# Load data from numpy file
+X =  np.load('feat.npy')
+y =  np.load('label.npy').ravel()
+#print(y)
+X_train=[]
+X_test=[]
+y_train=[]
+y_test=[]
+for i in range(len(X)):
+    if((i%3)!=0):
+        X_train.append(X[i])
+        y_train.append(y[i])
+    else:
+        X_test.append(X[i])
+        y_test.append(y[i])
+#print(X_test)
+
+print('fitting...')
+clf = SVC()
+clf.fit(X_train, y_train)
+print(clf.predict(X_test))
+#acc = clf.score(X_test, y_test)
+#print("acc=%0.3f" % acc)
